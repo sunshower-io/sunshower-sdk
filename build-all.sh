176bb908c43c8fa7ece80635e297952387170e69
josiahhaswell <josiah.haswell@gmail.com>
2017-12-27 13:07:58 -0700

    Removing password file

--- build-all.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-docker build -t sunshower-sdk -f Dockerfile .   
-docker run -e MVN_REPO_USERNAME=myMavenRepo \
--e MVN_REPO_PASSWORD=lid-DOG-bin-123 -it --rm \
--e MVN_REPO_URL=https://mymavenrepo.com/repo/IRgrTxMdF4OnnbNAkfnJ \
---name "sunshower-sdk" "sunshower-sdk" \
-
-
