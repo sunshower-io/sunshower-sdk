0e7e87b5d1ae161a49efb288ea41370f71f5f1e6
josiahhaswell <josiah.haswell@gmail.com>
2018-03-10 14:27:44 -0700

    Updating jdbctemplate

--- sdk-core/src/main/java/io/sunshower/sdk/v1/ExceptionMappings.java
+++ sdk-core/src/main/java/io/sunshower/sdk/v1/ExceptionMappings.java
@@ -14,152 +14,143 @@ import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
-import javax.ws.rs.ext.MessageBodyReader;
 import javax.ws.rs.ext.Provider;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
 
 @Provider
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 public class ExceptionMappings implements ExceptionMapper<Throwable> {
 
-    static final Logger logger = Logger.getLogger(ExceptionMappings.class.getName());
+  static final Logger logger = Logger.getLogger(ExceptionMappings.class.getName());
 
-    static final Map<Class<? extends Throwable>, ExceptionResponse> mappings = new HashMap<>();
+  static final Map<Class<? extends Throwable>, ExceptionResponse> mappings = new HashMap<>();
 
-    static {
-        register(
-                AuthenticationCredentialsNotFoundException.class,
-                new AuthenticationFailedResponse());
+  static {
+    register(AuthenticationCredentialsNotFoundException.class, new AuthenticationFailedResponse());
 
-        register(
-                AccessDeniedException.class,
-                new ForbiddenResponse());
+    register(AccessDeniedException.class, new ForbiddenResponse());
 
-        register(
-                InvalidCredentialException.class,
-                new AuthenticationFailedResponse());
+    register(InvalidCredentialException.class, new AuthenticationFailedResponse());
 
+    register(InvalidTokenException.class, new AuthenticationFailedResponse());
 
-        register(
-                InvalidTokenException.class,
-                new AuthenticationFailedResponse());
+    register(IllegalArgumentException.class, new BadRequestResponse());
 
-        register(IllegalArgumentException.class, new BadRequestResponse());
+    register(NoResultException.class, new NoSuchElementException());
 
-        register(
-                NoResultException.class,
-                new NoSuchElementException());
+    register(java.util.NoSuchElementException.class, new NoSuchElementException());
 
-        register(
-                java.util.NoSuchElementException.class,
-                new NoSuchElementException());
+    register(EntityNotFoundException.class, new NotFoundExceptionResponse());
 
-        register(
-                EntityNotFoundException.class,
-                new NotFoundExceptionResponse()
-        );
+    register(NotFoundException.class, new NotFoundExceptionResponse());
+  }
 
-        register(
-                NotFoundException.class,
-                new NotFoundExceptionResponse()
-        );
-        
+  public static Set<Map.Entry<Class<? extends Throwable>, ExceptionResponse>> getMappings() {
+    return mappings.entrySet();
+  }
+
+  static void register(Class<? extends Throwable> key, ExceptionResponse response) {
+    mappings.put(key, response);
+  }
+
+  @Override
+  public Response toResponse(Throwable exception) {
+    exception.printStackTrace();
+    if (exception instanceof WebApplicationException) {
+      return ((WebApplicationException) exception).getResponse();
     }
-
-
-    static void register(Class<? extends Throwable> key, ExceptionResponse response) {
-        mappings.put(key, response);
+    final ExceptionResponse exceptionResponse = mappings.get(exception.getClass());
+    if (exceptionResponse == null) {
+      logger.log(
+          Level.WARNING,
+          "Encountered an exception while processing REST request: {0}",
+          exception.getMessage());
+      logger.log(Level.WARNING, "Details:", exception);
+      return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
+          .entity(exception.getMessage())
+          .build();
     }
+    return exceptionResponse.create(exception);
+  }
 
-
-    @Override
-    public Response toResponse(Throwable exception) {
-        exception.printStackTrace();
-        if(exception instanceof WebApplicationException) {
-            return ((WebApplicationException) exception).getResponse();
-        }
-        final ExceptionResponse exceptionResponse = mappings.get(exception.getClass());
-        if(exceptionResponse == null) {
-            logger.log(Level.WARNING, "Encountered an exception while processing REST request: {0}", exception.getMessage());
-            logger.log(Level.WARNING, "Details:", exception);
-            return Response.status(
-                    Response.Status.INTERNAL_SERVER_ERROR)
-                    .entity(exception.getMessage()).build();
-        }
-        return exceptionResponse.create(exception);
-    }
-    public interface ExceptionResponse {
-        Response create(Throwable throwable);
-    }
+  public interface ExceptionResponse {
+    Response create(Throwable throwable);
+  }
 }
 
 class BadRequestResponse implements ExceptionMappings.ExceptionResponse {
-    @Override
-    public Response create(Throwable throwable) {
-        return Response.status(Response.Status.BAD_REQUEST)
-                .type(MediaType.APPLICATION_JSON_TYPE)
-                .entity(new Message((throwable.getMessage()))).build();
-    }
+  @Override
+  public Response create(Throwable throwable) {
+    return Response.status(Response.Status.BAD_REQUEST)
+        .type(MediaType.APPLICATION_JSON_TYPE)
+        .entity(new Message((throwable.getMessage())))
+        .build();
+  }
 }
 
 class NotFoundExceptionResponse implements ExceptionMappings.ExceptionResponse {
 
-    @Override
-    public Response create(Throwable throwable) {
-        return Response.status(Response.Status.NOT_FOUND)
-                .type(MediaType.APPLICATION_JSON_TYPE)
-                .entity(new Message((throwable.getMessage()))).build();
-    }
+  @Override
+  public Response create(Throwable throwable) {
+    return Response.status(Response.Status.NOT_FOUND)
+        .type(MediaType.APPLICATION_JSON_TYPE)
+        .entity(new Message((throwable.getMessage())))
+        .build();
+  }
 }
-
 
 class RepositoryExceptionResponse implements ExceptionMappings.ExceptionResponse {
 
-    @Override
-    public Response create(Throwable throwable) {
-        return Response.status(Response.Status.BAD_REQUEST)
-                .type(MediaType.APPLICATION_JSON_TYPE)
-                .entity(new Message(throwable.getMessage())).build();
-    }
+  @Override
+  public Response create(Throwable throwable) {
+    return Response.status(Response.Status.BAD_REQUEST)
+        .type(MediaType.APPLICATION_JSON_TYPE)
+        .entity(new Message(throwable.getMessage()))
+        .build();
+  }
 }
 
-
 class NoSuchElementException implements ExceptionMappings.ExceptionResponse {
-    @Override
-    public Response create(Throwable throwable) {
-        return Response.status(Response.Status.NOT_FOUND)
-                .type(MediaType.APPLICATION_JSON_TYPE)
-                .entity(new Message(throwable.getMessage())).build();
-    }
+  @Override
+  public Response create(Throwable throwable) {
+    return Response.status(Response.Status.NOT_FOUND)
+        .type(MediaType.APPLICATION_JSON_TYPE)
+        .entity(new Message(throwable.getMessage()))
+        .build();
+  }
 }
 
 class ConstraintViolationResponse implements ExceptionMappings.ExceptionResponse {
-    @Override
-    public Response create(Throwable throwable) {
-        return Response.status(Response.Status.BAD_REQUEST)
-                .type(MediaType.APPLICATION_JSON_TYPE)
-                .entity(new Message(throwable.getMessage())).build();
-    }
+  @Override
+  public Response create(Throwable throwable) {
+    return Response.status(Response.Status.BAD_REQUEST)
+        .type(MediaType.APPLICATION_JSON_TYPE)
+        .entity(new Message(throwable.getMessage()))
+        .build();
+  }
 }
+
 class AuthenticationFailedResponse implements ExceptionMappings.ExceptionResponse {
-    @Override
-    public Response create(Throwable throwable) {
-        return Response.status(Response.Status.UNAUTHORIZED)
-                .type(MediaType.APPLICATION_JSON_TYPE)
-                .entity(new Message(throwable.getMessage())).build();
-    }
+  @Override
+  public Response create(Throwable throwable) {
+    return Response.status(Response.Status.UNAUTHORIZED)
+        .type(MediaType.APPLICATION_JSON_TYPE)
+        .entity(new Message(throwable.getMessage()))
+        .build();
+  }
 }
 
 class ForbiddenResponse implements ExceptionMappings.ExceptionResponse {
-    @Override
-    public Response create(Throwable throwable) {
-        return Response.status(Response.Status.FORBIDDEN)
-                .type(MediaType.APPLICATION_JSON_TYPE)
-                .entity(new Message(throwable.getMessage())).build();
-    }
+  @Override
+  public Response create(Throwable throwable) {
+    return Response.status(Response.Status.FORBIDDEN)
+        .type(MediaType.APPLICATION_JSON_TYPE)
+        .entity(new Message(throwable.getMessage()))
+        .build();
+  }
 }
--- sdk-core/src/test/java/io/sunshower/sdk/test/SdkTestConfiguration.java
+++ sdk-core/src/test/java/io/sunshower/sdk/test/SdkTestConfiguration.java
@@ -4,15 +4,26 @@ import io.sunshower.kernel.api.PluginManager;
 import io.sunshower.persistence.Dialect;
 import io.sunshower.service.model.io.FileResolutionStrategy;
 import io.sunshower.test.common.TestConfigurations;
+import io.sunshower.test.persist.ConnectionDetectingJDBCTemplate;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.jdbc.core.JdbcTemplate;
 
+import javax.sql.DataSource;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
 
 @Configuration
 public class SdkTestConfiguration {
+    
+    @Bean
+    @Primary
+    public JdbcTemplate jdbcTemplate(DataSource datasource) {
+        return new ConnectionDetectingJDBCTemplate(datasource);
+    }
+    
+    
     
     @Bean
     public PluginManager pluginManager() {
