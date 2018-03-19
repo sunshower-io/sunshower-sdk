a569b9a29e6550a77fb488f49fa994d36ce958c9
josiahhaswell <josiah.haswell@gmail.com>
2018-03-18 20:17:38 -0600

    Fixing stuff

--- sdk-api/src/main/java/io/sunshower/sdk/v1/model/core/element/PropertyElement.java
+++ sdk-api/src/main/java/io/sunshower/sdk/v1/model/core/element/PropertyElement.java
@@ -7,7 +7,6 @@ import org.eclipse.persistence.oxm.XMLRoot;
 import javax.xml.bind.Unmarshaller;
 import javax.xml.bind.annotation.XmlAnyElement;
 import javax.xml.bind.annotation.XmlAttribute;
-import javax.xml.bind.annotation.XmlTransient;
 import java.lang.reflect.Method;
 import java.lang.reflect.ParameterizedType;
 import java.util.stream.Stream;
@@ -23,10 +22,10 @@ public class PropertyElement<U, T extends PropertyElement<U, T>> extends Abstrac
   @XmlAnyElement(lax = true)
   private U value;
 
-  private final transient Method valueOf;
+  private static transient Method valueOf;
 
   public PropertyElement() {
-    this.valueOf = configure();
+    this.valueOf = configure(getClass());
   }
 
   public PropertyElement(String key, String name, U value) {
@@ -35,13 +34,13 @@ public class PropertyElement<U, T extends PropertyElement<U, T>> extends Abstrac
     this.name = name;
     this.value = value;
   }
-  
-  
 
   @SuppressWarnings("unchecked")
   protected void doUnmarshal() {
     try {
-      this.value = (U) valueOf.invoke(this, ((XMLRoot) value).getObject());
+      if (valueOf != null) {
+        this.value = (U) valueOf.invoke(this, ((XMLRoot) value).getObject());
+      }
     } catch (Exception ex) {
       throw new RuntimeException(ex);
     }
@@ -52,21 +51,29 @@ public class PropertyElement<U, T extends PropertyElement<U, T>> extends Abstrac
     doUnmarshal();
   }
 
-  protected Method configure() {
-    final ParameterizedType superclass = (ParameterizedType) getClass().getGenericSuperclass();
-    final Class<?> valueType = (Class<?>) superclass.getActualTypeArguments()[0];
-    return Stream.of(valueType.getDeclaredMethods())
-        .filter(t -> t.getName().equals("valueOf") && t.getParameterCount() == 1)
-        .filter(
-            t -> {
-              final Class<?>[] ptypes = t.getParameterTypes();
-              final Class<?> ptype = ptypes[0];
-              return String.class.equals(ptype) || Object.class.equals(ptype);
-            })
-        .findFirst()
-        .orElseThrow(
-            () ->
-                new IllegalArgumentException(
-                    "Type does not supply a public, static method valueOf() accepting a string"));
+  protected static Method configure(Class<?> type) {
+    if (valueOf != null) {
+      return valueOf;
+    }
+    final ParameterizedType superclass = (ParameterizedType) type.getGenericSuperclass();
+    final Object vt = superclass.getActualTypeArguments()[0];
+
+    if (vt.getClass().equals(Class.class)) {
+      Class<?> valueType = (Class<?>) vt;
+      return Stream.of(valueType.getDeclaredMethods())
+          .filter(t -> t.getName().equals("valueOf") && t.getParameterCount() == 1)
+          .filter(
+              t -> {
+                final Class<?>[] ptypes = t.getParameterTypes();
+                final Class<?> ptype = ptypes[0];
+                return String.class.equals(ptype) || Object.class.equals(ptype);
+              })
+          .findFirst()
+          .orElseThrow(
+              () ->
+                  new IllegalArgumentException(
+                      "Type does not supply a public, static method valueOf() accepting a string"));
+    }
+    return null;
   }
 }
