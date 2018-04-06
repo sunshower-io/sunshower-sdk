ff2d5cd15fe6032547daa880c500df5d0f9bfe88
Josiah Haswell <josiah.haswell@gmail.com>
2018-04-06 10:09:33 -0600

    Adding verbose flag to build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,7 +5,7 @@ source /scripts/set-version.sh
 
 mvn clean install deploy -f bom/pom.xml -Denv.version=1.0.0-SNAPSHOT
 #mvn versions:set -DnewVersion=${VERSION} -f bom/pom.xml
-gradle clean build pTML publish \
+gradle clean build pTML publish -i \
 -PmavenRepositoryUrl=${MVN_REPO_URL} \
 -PmavenRepositoryUsername=${MVN_REPO_USERNAME} \
 -PmavenRepositoryPassword=${MVN_REPO_PASSWORD} \
